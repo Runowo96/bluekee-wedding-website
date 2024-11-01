b54f2eb5c5fadc7ea4d4cb825a3ec70615d5f64c
Devin Runowski <devinmrunowski@gmail.com>
2024-11-01 11:14:19 -0400

    added navbar button scroll functionality, and made styling breakpoint for a la carte section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import "./App.scss";
 import Hero from "./Components/Hero/Hero";
 import NavBar from "./Components/Navbar/NavBar";
@@ -5,17 +6,37 @@ import Packages from "./Components/Packages/Packages";
 import ProductLines from "./Components/ProductLines/ProductLines";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const heroRef = useRef(null);
+  const packagesRef = useRef(null);
+  const productLinesRef = useRef(null);
+
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    setIsOpen(false)
+  };
+
   return (
     <div>
-      <header>
+      <header className="test">
         <nav>
-          <NavBar />
+          <NavBar
+            scrollToSection={scrollToSection}
+            sections={{ heroRef, packagesRef, productLinesRef }}isOpen={isOpen}setIsOpen={setIsOpen}
+          />
         </nav>
       </header>
-      <main>
-        <Hero />
-        <Packages />
-        <ProductLines />
+      <main className="test2">
+        <section id="home" ref={heroRef}>
+          <Hero />
+        </section>
+        <section id="packages" ref={packagesRef}>
+          <Packages />
+        </section>
+        <section id="product-lines" ref={productLinesRef}>
+          <ProductLines />
+        </section>
       </main>
     </div>
   );
