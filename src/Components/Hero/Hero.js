84fb1abb57ddcaf6f3e972c40df5363d3b7096b9
Devin Runowski <devinmrunowski@gmail.com>
2024-11-01 11:34:54 -0400

    add meta tags

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,14 +5,14 @@ function Hero() {
   return (
     <section className='hero'>
       <article className='hero__text-bg'>
-      <h1 className='hero__header'>
+      {/* <h1 className='hero__header'>
         BLUE KEE DESIGN CO.
+      </h1> */}
+      <h1 className='hero__h2-text'>
+      Custom Laser-Cut Designs for Weddings and Events in Toronto
       </h1>
-      <h2 className='hero__h2-text'>
-        Unforgettable Details for Your Perfect Day
-      </h2>
       <h3 className='hero__h3-text'>
-        Customized laser-cut and engraved décor that tells your story.
+      Your personalized laser-cut designs for memorable events.
       </h3>
       <button className='hero__btn'>
         <h3 className='hero__btn-text'>Let Us Make Your Day Yours</h3>
