b6e5f9d87544a8481eb7a0529f78cf305dace9b4
Devin Runowski <devinmrunowski@gmail.com>
2024-11-03 10:25:27 -0500

    reduce box and text shadows reatlt, adjusted button animation

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./NavBar.scss";
 import menuIcon from "../../assets/icons/menu-icon.svg";
 import logo from "../../assets/logos/bk-logo.svg";
 
 function NavBar({ isOpen, setIsOpen, sections, scrollToSection }) {
+  // Disable body scrolling when the menu is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    // Cleanup to reset overflow when the component unmounts
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  const handleItemClick = (sectionRef) => {
+    scrollToSection(sectionRef);
+    setIsOpen(false); // Close menu after navigating
+  };
+
+  // Render menu items
+  const renderMenuItems = () => (
+    <>
+      <li className="navbar__list-item">
+        <h2
+          className="navbar__list-item-text"
+          onClick={() => handleItemClick(sections.packagesRef)}
+        >
+          Packages
+        </h2>
+      </li>
+      <li className="navbar__list-item">
+        <h2
+          className="navbar__list-item-text"
+          onClick={() => handleItemClick(sections.productLinesRef)}
+        >
+          A La Carte
+        </h2>
+      </li>
+      <li className="navbar__list-item">
+        <h2
+          className="navbar__list-item-text"
+          onClick={() => handleItemClick(sections.artistsRef)}
+        >
+          The Artists
+        </h2>
+      </li>
+      <li className="navbar__list-item">
+        <h2
+          className="navbar__list-item-text"
+          onClick={() => handleItemClick(sections.contactUsRef)}
+        >
+          Contact Us
+        </h2>
+      </li>
+    </>
+  );
+
   return (
     <div className="navbar">
       <button onClick={() => setIsOpen(!isOpen)} className="navbar__menu-btn">
@@ -14,104 +70,12 @@ function NavBar({ isOpen, setIsOpen, sections, scrollToSection }) {
         />
       </button>
       <img className="navbar__logo-img" src={logo} alt="Blue Kee Design logo" />
-      {isOpen && (
-        <ul className="navbar__dropdown-ul">
-          {/* <li className="navbar__list-item">
-            <a className="navbar__list-item-text" href="#home">
-              <h2
-                className="navbar__list-item-text"
-                onClick={() => scrollToSection(sections.heroRef)}
-              >
-                Home
-              </h2>
-            </a>
-          </li> */}
-          <li className="navbar__list-item">
-            {/* <a className="navbar__list-item-text" href="#packages"> */}
-            <h2
-              className="navbar__list-item-text"
-              onClick={() => scrollToSection(sections.packagesRef)}
-            >
-              Packages
-            </h2>
-            {/* </a> */}
-          </li>
-          <li className="navbar__list-item">
-            {/* <a className="navbar__list-item-text" href="#product-lines"> */}
-            <h2
-              className="navbar__list-item-text"
-              onClick={() => scrollToSection(sections.productLinesRef)}
-            >
-              A La Carte
-            </h2>
-            {/* </a> */}
-          </li>
-          <li className="navbar__list-item">
-            <h2
-              className="navbar__list-item-text"
-              onClick={() => scrollToSection(sections.artistsRef)}
-            >
-              The Artists
-            </h2>
-          </li>
-          <li className="navbar__list-item">
-            <h2
-              className="navbar__list-item-text"
-              onClick={() => scrollToSection(sections.contactUsRef)}
-            >
-              Contact Us
-            </h2>
-          </li>
-        </ul>
-      )}
-      <ul className="navbar__dropdown-ul-desktop">
-        {/* <li className="navbar__list-item">
-        <a className="navbar__list-item-text" href="#home">
-              <h2
-                className="navbar__list-item-text"
-                onClick={() => scrollToSection(sections.heroRef)}
-              >
-                Home
-              </h2>
-            </a>
-        </li> */}
-        <li className="navbar__list-item">
-          {/* <a className="navbar__list-item-text" href="#packages"> */}
-          <h2
-            className="navbar__list-item-text"
-            onClick={() => scrollToSection(sections.packagesRef)}
-          >
-            Packages
-          </h2>
-          {/* </a>        */}
-        </li>
-        <li className="navbar__list-item">
-          {/* <a className="navbar__list-item-text" href="#product-lines"> */}
-          <h2
-            className="navbar__list-item-text"
-            onClick={() => scrollToSection(sections.productLinesRef)}
-          >
-            A La Carte
-          </h2>
-          {/* </a>         */}
-        </li>
-        <li className="navbar__list-item">
-          <h2
-            className="navbar__list-item-text"
-            onClick={() => scrollToSection(sections.artistsRef)}
-          >
-            The Artists
-          </h2>{" "}
-        </li>
-        <li className="navbar__list-item">
-          <h2
-            className="navbar__list-item-text"
-            onClick={() => scrollToSection(sections.contactUsRef)}
-          >
-            Contact Us
-          </h2>{" "}
-        </li>
-      </ul>
+
+      {/* Mobile Dropdown Menu */}
+      {isOpen && <ul className="navbar__dropdown-ul">{renderMenuItems()}</ul>}
+
+      {/* Desktop Navigation */}
+      <ul className="navbar__dropdown-ul-desktop">{renderMenuItems()}</ul>
     </div>
   );
 }
