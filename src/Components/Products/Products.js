fb0ca6bb6efecfe0cbadb6c6720dc228eb2bb353
Devin Runowski <devinmrunowski@gmail.com>
2024-11-03 18:28:30 -0500

    add icons to products component

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Products.scss";
 import ProductInfo from "../ProductInfo/ProductInfo";
 
-function Products({ data, name }) {
+function Products({ data, name, icon }) {
   const [activeProductId, setActiveProductId] = useState(null); // Track the active product by ID
 
   const openProductInfo = (id) => {
@@ -14,35 +14,37 @@ function Products({ data, name }) {
   };
 
   const nextProduct = () => {
-    const currentIndex = data.findIndex(item => item.id === activeProductId);
+    const currentIndex = data.findIndex((item) => item.id === activeProductId);
     const nextIndex = (currentIndex + 1) % data.length;
     setActiveProductId(data[nextIndex].id);
   };
   const lastProduct = () => {
-    const currentIndex = data.findIndex(item => item.id === activeProductId);
+    const currentIndex = data.findIndex((item) => item.id === activeProductId);
     const lastIndex = (currentIndex - 1 + data.length) % data.length;
     setActiveProductId(data[lastIndex].id);
   };
 
-
   return (
     <div className="products__outer-cont">
-      <h3 className="products__header">{name}</h3>
+      <div className="products__header-cont">
+      <img className="products__icon-img" src={icon} alt="icon" />
+        <h3 className="products__header">{name}</h3>
+      </div>
       <div className="products">
         {data.map((item) => (
           <div key={item.id} className="products__cont">
-            <img 
-              className="products__image" 
-              src={item.image} 
-              alt={item.name} 
+            <img
+              className="products__image"
+              src={item.image}
+              alt={item.name}
               onClick={() => openProductInfo(item.id)} // Open on image click
             />
             <h4 className="products__name">{item.name}</h4>
             {activeProductId === item.id && (
               <section>
-                <ProductInfo 
-                  data={item} 
-                  onNext={nextProduct} 
+                <ProductInfo
+                  data={item}
+                  onNext={nextProduct}
                   onPrev={lastProduct}
                   onClose={closeProductInfo} // Only close when X button is clicked
                 />
